34965e54219e4086d9b652665f7b8f2f6da8e5b0
mohammud anas <anastechverse@gmail.com>
2024-02-05 01:06:20 +0530

    setup our entry route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import userRouter from "./routes/user.router.js";
 const app = express();
 app.use(
   cors({
@@ -13,5 +14,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
