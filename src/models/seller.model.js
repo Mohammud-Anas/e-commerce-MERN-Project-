23a887093e4cb66013492d531205feb0a154edde
mohammud anas <anastechverse@gmail.com>
2024-02-14 21:26:18 +0530

    created seller schema

--- src/models/seller.model.js
+++ src/models/seller.model.js
@@ -1,26 +1,82 @@
-import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import mongoose, { Schema } from "mongoose";
+
 const sellerSchema = new Schema(
   {
     storename: {
       required: true,
       type: String,
     },
-    store_address: {
+    address: {
       required: true,
       type: String,
     },
-    store_pincode: {
+    pincode: {
       required: true,
       type: Number,
     },
-    store_owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
+    ownerName: {
+      type: String,
+    },
+
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
     },
   },
   {
     timestamps: true,
   }
 );
-
+sellerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+sellerSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+sellerSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      phoneNumber: this.phoneNumber,
+      ownerName: this.ownerName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+sellerSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const Seller = mongoose.model("Seller", sellerSchema);
