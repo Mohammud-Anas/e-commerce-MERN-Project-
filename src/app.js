f28f76ac2d10659a7498feb50ecce9cec6e338e9
mohammud anas <anastechverse@gmail.com>
2024-02-14 21:24:52 +0530

    added seller entry route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
+import sellerRouter from "./routes/seller.router.js";
 import userRouter from "./routes/user.router.js";
 const app = express();
 app.use(
@@ -16,5 +17,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/sellers", sellerRouter);
 
 export { app };
